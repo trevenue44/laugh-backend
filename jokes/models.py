e1ada105f583130bcb15f1133b1d667384c7c5ed
trevenue44 <trevenue44.main@gmail.com>
2023-04-26 22:44:46 +0000

    joke model

--- jokes/models.py
+++ jokes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from accounts.models import Laugher
+
+
 # Create your models here.
+class Joke(models.Model):
+    # the person who published the joke
+    laugher = models.ForeignKey(Laugher, on_delete=models.CASCADE)
+
+    # the content of the joke
+    content = models.CharField(max_length=125)
+    date_published = models.DateTimeField(auto_now_add=True, blank=True)
+    # the laughers who laughed at the joke. Number of laughs
+    laughs = models.ManyToManyField(Laugher, blank=True, related_name="laughs")
+
+    def __str__(self) -> str:
+        return self.content
